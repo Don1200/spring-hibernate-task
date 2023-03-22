1f5e81591872126c3f19fafefc18751e72e3fa6b
Александр <dpn1500@gmail.com>
2023-03-22 16:38:54 +0300

    V 1.2

---
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/CarDao.java
@@ -0,0 +1,7 @@
+package hiber.dao;
+
+import hiber.model.Car;
+
+public interface CarDao {
+    Car addCar(Car car);
+}
---
+++ 2.2.1. spring_hibernate/src/main/java/hiber/service/CarService.java
@@ -0,0 +1,9 @@
+package hiber.service;
+
+import hiber.model.Car;
+import org.springframework.stereotype.Service;
+
+
+public interface CarService {
+    Car addCar(Car car);
+}
